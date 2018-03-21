04d3e992d65cbb8caef868bdb60740c6145d1f4d
wfhsiao <wfhsiao@users.noreply.github.com>
2018-03-21 11:16:10 +0800

    排序及二分搜尋
    with Arrays.sort and binarySearch

--- MyReadFile.java
+++ MyReadFile.java
@@ -18,10 +18,19 @@ public class MyReadFile {
                 scores[i] = scan.nextInt();
                 i++;
             }
+            scan.close();
             System.out.println(Arrays.toString(scores));
             Arrays.sort(scores);
-            reverse(scores);
             System.out.println(Arrays.toString(scores));
+            scan = new Scanner(System.in);
+            System.out.print("請輸入欲搜尋的數值: ");
+            int n = scan.nextInt();
+            int index = Arrays.binarySearch(scores, n);
+//            reverse(scores);
+            if (index < 0)
+                System.out.printf("找不到%d!!\n", n);
+            else
+                System.out.printf("%d在位置%d.\n", n, index);
 //        System.out.print("請輸入一行字串: ");
 //        String line = scan.nextLine();
 //        System.out.printf("你輸入的是: %s\n", line);
