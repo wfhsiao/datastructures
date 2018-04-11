4b7ac3f4dc35510db5e8098351e5bec67035b9db
wfhsiao <wfhsiao@users.noreply.github.com>
2018-04-11 09:41:45 +0800

    add toString method

--- Student.java
+++ Student.java
@@ -10,8 +10,8 @@ public class Student {
         this.name=name;
         this.age=age;
     }
-    public static void main(String[] args) {
-        Student s = new Student("106406001", "張三", 20);
-        System.out.printf("s=(%s, %s, %d)\n", s.no, s.name, s.age);
+    // Arrays.toString
+    public String toString() {
+        return String.format("(%s, %s, %d)", no, name, age);
     }
 }
--- StudentTest.java
+++ StudentTest.java
@@ -3,5 +3,8 @@
  * @author user
  */
 public class StudentTest {
-    
+    public static void main(String[] args) {
+        Student s = new Student("106406001", "張三", 20);
+        System.out.printf("s=%s\n", s);
+    }
 }
