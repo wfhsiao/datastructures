5f00743b9dc11f8e63f370d8398d6fa15e12701d
wfhsiao <wfhsiao@users.noreply.github.com>
2018-05-02 10:45:34 +0800

    add stack array demo

--- MyQueueTest.java
+++ MyQueueTest.java
@@ -1,7 +1,9 @@
 
+import java.lang.reflect.Array;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
@@ -10,7 +12,8 @@ import java.util.Queue;
 public class MyQueueTest {
     public static void main(String[] args) {
 //        queue();
-          deque();
+//          deque();
+          stack();
     }
 
     private static void queue() {
@@ -41,5 +44,31 @@ public class MyQueueTest {
         System.out.printf("deque= %s\n", deque);
         System.out.println(deque.peekLast());
         System.out.printf("deque= %s\n", deque);
+    }
+
+    private static void stack() {
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer>[] stacks = (Stack<Integer>[]) Array.newInstance(Stack.class, 3);
+        for (int i = 0; i < stacks.length; i++) {
+            stacks[i] = new Stack<>();            
+        }
+        
+        stacks[0].push(-7);
+        stacks[0].push(-70);
+        stacks[1].push(-700);
+        stacks[2].push(-7000);
+        print(stacks);
+//        System.out.printf("stack= %s\n", stack);
+//        while (! stack.empty()) {            
+//            System.out.println(stack.pop());
+//            System.out.printf("stack= %s\n", stack);            
+//        }
+        
+    }
+
+    private static void print(Stack<Integer>[] stacks) {
+        for (int i = 0; i < stacks.length; i++) {
+            System.out.println(stacks[i]);            
+        }
     }
 }
