d757af679820ca98f8a947106cdafaf3d5b0225e
wfhsiao <wfhsiao@users.noreply.github.com>
2018-05-16 09:56:46 +0800

    ok

--- maze/MyMaze.java
+++ maze/MyMaze.java
@@ -14,14 +14,40 @@ public class MyMaze {
         try {
             File file = new File("mazedata2.txt");
             Scanner scan = new Scanner(file);
-            String line = scan.nextLine();
-            String str[]= line.split(" ");
-            int row = Integer.parseInt(str[0]);
-            int col = Integer.parseInt(str[1]);
-            System.out.printf("row=%d, col=%d\n", row, col);
+//            String line = scan.nextLine();
+//            String str[]= line.split(" ");
+            int row = scan.nextInt();
+            int col = scan.nextInt();
+//            System.out.printf("row=%d, col=%d\n", row, col);
+            int [][] maze = new int[row][col];
+            readMaze(scan, maze);
+            showMaze(maze);
         } catch (FileNotFoundException ex) {
             System.out.printf("error in opening file: %s\n", ex.getMessage());
         }
         
+    }
+
+    private static void readMaze(Scanner scan, int[][] maze) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                maze[i][j] = scan.nextInt();                
+            }            
+        }        
+    }
+
+    private static void showMaze(int[][] maze) {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                switch (maze[i][j])
+                {
+                    case 0: System.out.print("¡@");
+                            break;
+                    case 2: System.out.print("¢i");
+                            break;
+                }
+            }            
+            System.out.println();
+        } 
     }
 }
