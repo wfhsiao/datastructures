38f7fea04203efb76696cd00efe762b361bb9944
wfhsiao <wfhsiao@users.noreply.github.com>
2018-05-16 09:41:40 +0800

    ok

--- maze/MyMaze.java
+++ maze/MyMaze.java
@@ -1,13 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author user
  */
 public class MyMaze {
-    
+    public static void main(String[] args) {
+        try {
+            File file = new File("mazedata2.txt");
+            Scanner scan = new Scanner(file);
+            String line = scan.nextLine();
+            String str[]= line.split(" ");
+            int row = Integer.parseInt(str[0]);
+            int col = Integer.parseInt(str[1]);
+            System.out.printf("row=%d, col=%d\n", row, col);
+        } catch (FileNotFoundException ex) {
+            System.out.printf("error in opening file: %s\n", ex.getMessage());
+        }
+        
+    }
 }
