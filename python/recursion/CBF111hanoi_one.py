bdb73eb83fd2af112978a90ed6bf965cf19a3699
wfhsiao <wfhsiao@users.noreply.github.com>
2023-03-23 10:20:11 +0800

    Add files via upload

---
+++ python/recursion/CBF111hanoi_one.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 10:15:21 2023
+
+@author: user
+"""
+
+def hanoi(N, src, aux, des):
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    steps = hanoi(5, 'src', 'aux', 'des')
+    print(f'total steps: {steps}')
+    
