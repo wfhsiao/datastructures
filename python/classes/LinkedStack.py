b3d236776ecdd4aee2d5db0d3d4cde5cda354b75
wfhsiao <wfhsiao@users.noreply.github.com>
2023-04-02 15:36:58 +0800

    Update LinkedStack.py

--- python/classes/LinkedStack.py
+++ python/classes/LinkedStack.py
@@ -1,50 +1,59 @@
-# Python program for linked list implementation of stack 
-  
-# Class to represent a node 
-class StackNode: 
-  
-    # Constructor to initialize a node 
-    def __init__(self, data): 
-        self.data = data  
+# Python program for linked list implementation of stack
+
+class StackNode:
+    def __init__(self, data):
+        self.data = data
         self.next = None
-  
-class Stack: 
-      
-    # Constructor to initialize the root of linked list 
-    def __init__(self): 
-        self.root = None
-  
-    def isEmpty(self): 
-        return True if self.root is None else  False 
-  
-    def push(self, data): 
-        newNode = StackNode(data) 
-        newNode.next = self.root  
-        self.root = newNode 
-        #print(f"{data} pushed to the stack.") 
-      
-    def pop(self): 
-        if (self.isEmpty()): 
-            return None      # instead of float("-inf") 
-        temp = self.root  
-        self.root = self.root.next 
-        popped = temp.data 
-        return popped 
-      
-    def peek(self): 
-        if self.isEmpty(): 
-            return None      # instead of float("-inf") 
-        return self.root.data 
-  
-    def __str__(self):
-        p = self.root
-        res=[]
-        while p:
-            res.append(f'{p.data}')
-            p = p.next
-        res.reverse()
-        return '['+' '.join(res)
-
+    
     def __repr__(self):
-        return self.__str__()
+        return repr(self.data)
 
+
+class Stack:
+    def __init__(self):
+        self.root = None
+
+    def is_empty(self):
+        return self.root is None
+
+    def isEmpty(self):
+        return self.is_empty()
+
+    def push(self, data):
+        new_node = StackNode(data)
+        new_node.next = self.root
+        self.root = new_node
+
+    def pop(self):
+        if self.is_empty():
+            raise StackEmptyError("Cannot pop from an empty stack")
+        temp = self.root
+        self.root = self.root.next
+        popped = temp.data
+        return popped
+
+    def peek(self):
+        if self.is_empty():
+            raise StackEmptyError("Cannot peek into an empty stack")
+        return self.root.data
+        
+    def __repr__(self):
+        res = []
+        curr = self.root
+        while curr:
+            res.append(str(curr))
+            curr = curr.next
+        if not res:
+            return '['
+        return '[' + ', '.join(res[::-1])
+
+    def __len__(self):
+        curr = self.root
+        count = 0
+        while curr:
+            count += 1
+            curr = curr.next
+        return count
+      
+class StackEmptyError(Exception):
+    pass
